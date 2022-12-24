eeee0e53755aa8199dcc152ff506d0d03d3609e3
Daniel Riechers <driecehrs101@gmail.com>
2022-12-23 21:33:45 -0600

    Cleaned up fetch code

--- fetch.c
+++ fetch.c
@@ -4,26 +4,14 @@
 #include "map.h"
 
 // TODO error checking
-// TODO rewrite to download png to file_path
-void downloadRadarImage(struct map *map, unsigned long long time_offset, char *file_path) {
+void download_image(char *fmt, unsigned long long time, char *file_path) {
 	CURL *curl;
 	FILE *fp;
 	CURLcode res;
 	char url[512];
 	char fileName[11];
 
-	struct timeval tv;
-
-	gettimeofday(&tv, NULL);
-	unsigned long long millisecondsSinceEpoch =
-		(unsigned long long)(tv.tv_sec) * 1000 +
-		(unsigned long long)(tv.tv_usec) / 1000;
-
-	// TODO load format string from map
-	snprintf(url, 512,  "https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/export?dpi=96&transparent=true&format=png8&layers=show%%3A3&time=%llu%%2C1671846900000&bbox=-15290014.707599312%%2C2603370.6826554714%%2C-6083327.524708849%%2C7622531.70797195&bboxSR=3857&imageSR=3857&size=941%%2C513&f=image", millisecondsSinceEpoch - time_offset);
-	printf("%s\n", url);
-
-
+	snprintf(url, 512, fmt, time);
 
 	curl = curl_easy_init();
 	if (curl) {
@@ -38,15 +26,24 @@ void downloadRadarImage(struct map *map, unsigned long long time_offset, char *f
 	}
 }
 
+void download_radar_image(struct map *map, unsigned long long time, char *file_path)
+{
+	// TODO use position / url data from map
+	download_image("https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/export?dpi=96&transparent=true&format=png8&layers=show%%3A3&time=%llu%%2C1671846900000&bbox=-15290014.707599312%%2C2603370.6826554714%%2C-6083327.524708849%%2C7622531.70797195&bboxSR=3857&imageSR=3857&size=941%%2C513&f=image", time, file_path);
+}
+
 // TODO same as radar but with cloud cover
-//char* downloadCloudImage(struct map *map, int time_offset) {
+//downloadCloudImage
 
 int main(void)
 {
-	downloadRadarImage(NULL, 0, "test.png");
-	downloadRadarImage(NULL, 3600000, "test1.png");
-	downloadRadarImage(NULL, 3600000*2, "test2.png");
-	downloadRadarImage(NULL, 3600000*3, "test3.png");
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	unsigned long long millisecondsSinceEpoch =
+		(unsigned long long)(tv.tv_sec) * 1000 +
+		(unsigned long long)(tv.tv_usec) / 1000;
+
+	download_radar_image(NULL, millisecondsSinceEpoch, "test.png");
 
 	return 0;
 }
