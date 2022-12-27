377e52086b994c88fd8108d016f2e7bbcd987efb
Daniel Riechers <driecehrs101@gmail.com>
2022-12-27 15:50:25 -0600

    Cleanup
    Just cleaned up indentation and comments mostly
    Fixed an extra free

--- readpng.c
+++ readpng.c
@@ -25,99 +25,82 @@
 #include "readpng.h"
 
 struct png_data read_png_file(char *filename) {
-  int width, height;
-  png_byte color_type;
-  png_byte bit_depth;
-  png_bytep *row_pointers;
-  FILE *fp = fopen(filename, "rb");
+	int width, height;
+	png_byte color_type;
+	png_byte bit_depth;
+	png_bytep *row_pointers;
+	FILE *fp = fopen(filename, "rb");
 
-  png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if(!png) abort();
+	png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if(!png) abort();
 
-  png_infop info = png_create_info_struct(png);
-  if(!info) abort();
+	png_infop info = png_create_info_struct(png);
+	if(!info) abort();
 
-  //if(setjmp(png_jmpbuf(png))) abort();
+	if(setjmp(png_jmpbuf(png))) abort();
 
-  png_init_io(png, fp);
+	png_init_io(png, fp);
 
-  png_read_info(png, info);
+	png_read_info(png, info);
 
-  width      = png_get_image_width(png, info);
-  height     = png_get_image_height(png, info);
-  color_type = png_get_color_type(png, info);
-  bit_depth  = png_get_bit_depth(png, info);
+	width      = png_get_image_width(png, info);
+	height     = png_get_image_height(png, info);
+	color_type = png_get_color_type(png, info);
+	bit_depth  = png_get_bit_depth(png, info);
 
-  // Read any color_type into 8bit depth, RGBA format.
-  // See http://www.libpng.org/pub/png/libpng-manual.txt
+	// Read any color_type into 8bit depth, RGBA format.
+	// See http://www.libpng.org/pub/png/libpng-manual.txt
 
-  if(bit_depth == 16)
-    png_set_strip_16(png);
+	if(bit_depth == 16)
+		png_set_strip_16(png);
 
-  if(color_type == PNG_COLOR_TYPE_PALETTE)
-    png_set_palette_to_rgb(png);
+	if(color_type == PNG_COLOR_TYPE_PALETTE)
+		png_set_palette_to_rgb(png);
 
-  // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
-  if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-    png_set_expand_gray_1_2_4_to_8(png);
+	// PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
+	if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+		png_set_expand_gray_1_2_4_to_8(png);
 
-  if(png_get_valid(png, info, PNG_INFO_tRNS))
-    png_set_tRNS_to_alpha(png);
+	if(png_get_valid(png, info, PNG_INFO_tRNS))
+		png_set_tRNS_to_alpha(png);
 
-  // These color_type don't have an alpha channel then fill it with 0xff.
-  if(color_type == PNG_COLOR_TYPE_RGB ||
-     color_type == PNG_COLOR_TYPE_GRAY ||
-     color_type == PNG_COLOR_TYPE_PALETTE)
-    png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+	// These color_type don't have an alpha channel then fill it with 0xff.
+	if(color_type == PNG_COLOR_TYPE_RGB ||
+	   color_type == PNG_COLOR_TYPE_GRAY ||
+	   color_type == PNG_COLOR_TYPE_PALETTE)
+		png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
 
-  if(color_type == PNG_COLOR_TYPE_GRAY ||
-     color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
-    png_set_gray_to_rgb(png);
+	if(color_type == PNG_COLOR_TYPE_GRAY ||
+	   color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+		png_set_gray_to_rgb(png);
 
-  png_read_update_info(png, info);
+	png_read_update_info(png, info);
 
-  row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
-  for(int y = 0; y < height; y++) {
-    row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
-    //row_pointers[y] = png_malloc(png, png_get_rowbytes(png,info));
-  }
+	row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
+	for(int y = 0; y < height; y++)
+		row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
 
-  png_read_image(png, row_pointers);
+	png_read_image(png, row_pointers);
 
-  fclose(fp);
-  struct png_data ret = {
-	  png,
-	  info,
-	  row_pointers
-  };
+	fclose(fp);
+	struct png_data ret = {
+		png,
+		info,
+		row_pointers
+	};
 
-  return ret;
+	return ret;
 }
 
 void free_image(struct png_data png_data)
 {
-  int width, height;
-  width      = png_get_image_width(png_data.png, png_data.info);
-  height     = png_get_image_height(png_data.png, png_data.info);
-  for(int y=0; y < height; y++)
-    free(png_data.image[y]);
-  free(png_data.image);
-  png_destroy_info_struct(png_data.png, &png_data.info);
-  png_destroy_read_struct(&png_data.png, NULL, NULL);
-  free(png_data.info);
+	int width      = png_get_image_width(png_data.png, png_data.info);
+	int height     = png_get_image_height(png_data.png, png_data.info);
+
+	for(int y=0; y < height; y++)
+		free(png_data.image[y]);
+	free(png_data.image);
+
+	png_destroy_info_struct(png_data.png, &png_data.info);
+	png_destroy_read_struct(&png_data.png, NULL, NULL);
 }
-/*
-int main(int argc, char *argv[]) {
-  if(argc != 2) abort();
-
-  png_bytep *row_pointers = read_png_file(argv[1]);
-
-  int x  = 146;
-  int y  = 157;
-  png_bytep row = row_pointers[y];
-  png_bytep px = &(row[x * 4]);
-  printf("%4d, %4d = RGBA(%3d, %3d, %3d, %3d)\n", x, y, px[0], px[1], px[2], px[3]);
-
-  return 0;
-}*/
-
--- vt_one_hundrify.c
+++ vt_one_hundrify.c
@@ -102,33 +102,13 @@ const char* color_to_vt100(enum color color)
 	return "";
 }
 
-/*void libpng_init (void)
-{
-	pngptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    	pnginfo = png_create_info_struct(pngptr);
-        setjmp(png_jmpbuf(pngptr));
-	//png_set_palette_to_rgb(pngptr);
-	//png_set_expand(pngptr);
-	//png_set_tRNS_to_alpha(pngptr);
-   //png_colorp palette = (png_colorp)png_malloc(pngptr, 256 * sizeof (png_color));
-   //png_set_PLTE(pngptr, pnginfo, palette, 256);
-}*/
-
 int vt_one_hundrify(struct map *map, char *filename)
 {
+	int h = map->hpx;
+	int w = map->wpx;
+	int mH = map->h;
+	int mW = map->w;
 	struct png_data png = read_png_file(filename);
-/*
-	FILE *fp = fopen(filename, "rb");
-	png_init_io(pngptr, fp);
-        png_read_info(pngptr, pnginfo);
-
-	png_bytepp rows;
-	png_read_png(pngptr, pnginfo, PNG_TRANSFORM_IDENTITY, NULL);
-	printf("got hereeeee\n");
-	rows = png_get_rows(pngptr, pnginfo);
-	printf("got hereeeee\n");
-	fclose(fp);
-*/
 
 	if (4 != png_get_channels(png.png, png.info))
 		return -1; // not 4 channels
@@ -137,75 +117,51 @@ int vt_one_hundrify(struct map *map, char *filename)
 	if (map->wpx != png_get_image_width(png.png, png.info))
 		return -3; // not the expected height
 
-	/*for (int i = 0; i < map->hpx; i++) {
-        for (int j = 0; j < map->wpx * 4; j += 4) {
-		int hist[6] = { 0, 0, 0, 0, 0, 0 };
-		if (rows[i][j + 3] == 255) {
-			struct HSV hsv = rgb_to_hsv(rows[i][j], rows[i][j + 1], rows[i][j + 2]);
-			printf("%s\n", hue_to_vt100(hsv.H));
-			//printf("%f %f %f\n", hsv.H, hsv.S, hsv.V);
+	for(int row=0; row < mH; row++)
+	for(int col=0; col < mW; col++) {
+		int count[color_count];
+		memset(count, 0, color_count*sizeof(int));
+
+		for(int y=row*h/mH; y < (row+1)*h/mH; y++)
+		for(int x=col*w/mW; x < (col+1)*w/mW; x++) {
+			png_bytep img_row = png.image[y];
+			png_bytep px = &(img_row[x * 4]);
+			int r= px[0];
+			int b= px[1];
+			int g= px[2];
+			int a= px[3];
+			if(a== 255) {
+				double h = rgb_to_hsv(r, b, g).H;
+
+				if (hue_is_vt100_red(h))
+					count[red]++;
+				else if (hue_is_vt100_green(h))
+					count[green]++;
+				else if (hue_is_vt100_yellow(h))
+					count[yellow]++;
+				else if (hue_is_vt100_blue(h))
+					count[blue]++;
+				else if (hue_is_vt100_magenta(h))
+					count[magenta]++;
+				else if (hue_is_vt100_cyan(h))
+					count[cyan]++;
+			}
 		}
-        } */
-    int h = map->hpx;
-    int w = map->wpx;
-    int mH = map->h;
-    int mW = map->w;
-    //printf("color count %d\n", color_count);
-    for(int row=0; row < mH; row++) {
-        for(int col=0; col < mW; col++) {
-            int count[color_count];
-            memset(count, 0, color_count*sizeof(int));
-    //printf("row: %d col: %d map->w %d\n", row, col, map->w);
-    //fflush(stdout);
 
-            for(int y=row*h/mH; y < (row+1)*h/mH; y++) {
-                for(int x=col*w/mW; x < (col+1)*w/mW; x++) {
-  			  png_bytep img_row = png.image[y];
-                          png_bytep px = &(img_row[x * 4]);
-			  int r= px[0];
-			  int b= px[1];
-			  int g= px[2];
-			  int a= px[3];
-                          if(a== 255) {
+		int max=0;
+		enum color color=0;
+		for(int i=0; i<color_count;i++) {
+			if(count[i] > max){
+				max = count[i];
+				color = i;
+			}
+		}
 
-                          //if(rows[y][x+3] > 0 && rows[y][x+3] < 253 || rows[y][x+3] > 253) {
-                              double h = rgb_to_hsv(r, b, g).H;
-		          //printf("x: %d y: %d r: %d g: %d b: %d a: %d\n", x/4, y, rows[y][x+0], rows[y][x+1], rows[y][x+2], rows[y][x+3]);
-
-                              if (hue_is_vt100_red(h))
-                                  count[red]++;
-                              else if (hue_is_vt100_green(h))
-                                  count[green]++;
-                              else if (hue_is_vt100_yellow(h))
-                                  count[yellow]++;
-                              else if (hue_is_vt100_blue(h))
-                                  count[blue]++;
-                              else if (hue_is_vt100_magenta(h))
-                                  count[magenta]++;
-                              else if (hue_is_vt100_cyan(h))
-                                  count[cyan]++;
-			  }
-                }
-            }
-
-            int max=0;
-            enum color color=0;
-            for(int i=0; i<color_count;i++) {
-                if(count[i] > max){
-                    max = count[i];
-                    color = i;
-                }
-            }
-
-            if(max != 0) {
-		map->radar[row*map->w + col] = color;
-            }
-            else {
-		//printf("%d %d %d colored\n", col, row, map->w );
-		map->radar[row*map->w + col] = normal;
-            }
-        }
-    }
-    free_image(png);
-    return 0;
+		if(max != 0)
+			map->radar[row*map->w + col] = color;
+		else
+			map->radar[row*map->w + col] = normal;
+	}
+	free_image(png);
+	return 0;
 }
